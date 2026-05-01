c15b3344105907d1efb2c6de3527a42b629a2f27
AnnieInstinct <andja.gjoka@gmail.com>
2026-05-01 13:45:12 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
     // Send email via Resend
     const { data: emailData, error: emailError } = await resend.emails.send({
       from: 'Your Angel Study <access@yourangelstudy.com>',
-      to: [customerEmail],
+      reply_to: 'yourangelstudy@protonmail.com',
       subject: '🎴 Your PMP™ Flashcards - Access Token Inside',
       html: htmlContent,
     });
